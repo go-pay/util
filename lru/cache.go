3698df176962538e393ef02dedd0b33364d87849
Jerry <85411418@qq.com>
2024-09-24 01:09:20 +0800

    update

--- lru/cache.go
+++ lru/cache.go
@@ -2,13 +2,15 @@ package lru
 
 import "sync"
 
+// Entry 缓存中的实体
 type Entry struct {
-	Key   string
-	Value any
-	pre   *Entry
-	next  *Entry
+	Key   string // 键
+	Value any    // 值
+	pre   *Entry // 前一个实体
+	next  *Entry // 后一个实体
 }
 
+// Cache LRU 缓存
 type Cache struct {
 	mu    sync.RWMutex
 	cache map[string]*Entry
@@ -17,81 +19,122 @@ type Cache struct {
 	tail  *Entry
 }
 
+// NewCache 创建一个新的 LRU 缓存
 func NewCache(cap int) *Cache {
 	return &Cache{cache: make(map[string]*Entry), cap: cap}
 }
 
+// Put 将一个键值对放入缓存
 func (c *Cache) Put(key string, v any) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	// put this key exist, update index hot
+	// 如果键已存在，更新值并将其移动到头部
 	if e, has := c.cache[key]; has {
-		e.Key = key
 		e.Value = v
 		c.moveToHead(e)
 		return
 	}
-	// generate Entry
+	// 创建一个新实体
 	entry := &Entry{Key: key, Value: v, pre: nil, next: c.head}
-	// put new Entry
+	// 将新实体放入头部
 	c.putNewEntryHead(entry)
-	// not full
-	if len(c.cache) <= c.cap {
-		return
+	// 如果缓存已满，移除尾部实体
+	if len(c.cache) > c.cap {
+		c.removeTail()
 	}
-	// full, remove tail entry
-	c.removeTail()
 }
 
+// Get 获取键对应的值
 func (c *Cache) Get(key string) (v any) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
+	// 如果键存在，移动到头部并返回值
 	if e, has := c.cache[key]; has {
-		// link to head
 		c.moveToHead(e)
 		return e.Value
 	}
 	return nil
 }
 
+// moveToHead 将实体移动到链表头部
+//func (c *Cache) moveToHead(e *Entry) {
+//	// 如果条目已经在头部，直接返回
+//	if e == c.head {
+//		return
+//	}
+//	// 将 e 自身从链表中当前位置摘除
+//	// e.pre link e.next(entry or nil)
+//	e.pre.next = e.next
+//	if e == c.tail {
+//		// if e is tail, c.tail = e.pre
+//		c.tail = e.pre
+//	}
+//
+//	// 将 e 自身放入链表头部
+//	c.head.pre = e  // 原头部节点的前指针指向新节点
+//	c.head = e      // 原头部节点替换成新节点
+//	e.next = c.head // 新节点的后指针指向头部节点
+//	e.pre = nil     // 新节点的前指针置空
+//}
+
+// moveToHead 将实体移动到链表头部
 func (c *Cache) moveToHead(e *Entry) {
+	// 如果实体已经在头部，直接返回
 	if e == c.head {
-		// already in front, return
 		return
 	}
-	// 将 e 自身从链表中当前位置摘除
-	// e.pre link e.next(entry or nil)
-	e.pre.next = e.next
+	// 将实体从当前位置摘除
+	if e.pre != nil {
+		e.pre.next = e.next
+	}
+	if e.next != nil {
+		e.next.pre = e.pre
+	}
+	// 如果e是最后一个实体，设置尾部为e的前一个实体
 	if e == c.tail {
-		// if e is tail, c.tail = e.pre
 		c.tail = e.pre
 	}
-
-	// 将 e 自身放入链表头部
-	c.head.pre = e  // 原头部节点的前指针指向新节点
-	c.head = e      // 原头部节点替换成新节点
-	e.next = c.head // 新节点的后指针指向头部节点
-	e.pre = nil     // 新节点的前指针置空
-}
-
-func (c *Cache) putNewEntryHead(e *Entry) {
-	// link to head
+	// 将e实体放入链表头部
+	e.next = c.head
+	e.pre = nil
 	if c.head != nil {
 		c.head.pre = e
 	}
-	// reset head entry
 	c.head = e
+	// 如果链表尾部为空，设置尾部为当前实体e
 	if c.tail == nil {
-		// tail == nil, stack is empty
 		c.tail = e
 	}
+}
+
+func (c *Cache) putNewEntryHead(e *Entry) {
+	// 如果头部不为空，将头部前指针指向新实体
+	if c.head != nil {
+		c.head.pre = e
+	}
+	// 设置新实体为头部
+	c.head = e
+	// 如果尾部为空，设置尾部为新实体
+	if c.tail == nil {
+		c.tail = e
+	}
+	// 将新实体放入缓存
 	c.cache[e.Key] = e
 }
 
 func (c *Cache) removeTail() {
+	// 如果尾部为空，直接返回
+	if c.tail == nil {
+		return
+	}
+	// 获取尾部实体
 	removeEntry := c.tail
+	// 将尾部前移
 	c.tail = c.tail.pre
-	removeEntry.pre = nil
-	c.tail.next = nil
+	// 如果尾部不为空，将尾部的后指针置空
+	if c.tail != nil {
+		c.tail.next = nil
+	}
+	// 从缓存中删除尾部实体
 	delete(c.cache, removeEntry.Key)
 }
